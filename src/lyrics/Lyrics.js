1544228d45abe5b355df929693961ed8edfd4738
Thiago Marinho <tgmarinho@gmail.com>
2018-08-23 15:12:24 -0400

    changed for onChange

--- src/lyrics/Lyrics.js
+++ src/lyrics/Lyrics.js
@@ -4,13 +4,12 @@ import Fade from "react-reveal/Fade";
 import NavBar from "../components/NavBar";
 import Pulse from "react-reveal/Pulse";
 import Footer from "../components/Footer";
-
 import { Input, Form } from 'antd';
 
 const { Search, TextArea } = Input;
-
 const FormItem = Form.Item;
 
+// API Key do Vagalume
 const key = "0efde0cb9c9f9dde0f3b8be3251063f0";
 
 
@@ -18,14 +17,13 @@ const getLyricsById = async (id) => {
 
     const queryById = `https://api.vagalume.com.br/search.php?musid=${id}&apikey=${key}`
 
-
     return await axios
         .get(
             queryById
         )
         .then(data => {
-            console.log("getLyricsById", data);
-            console.log({ data });
+            // console.log("getLyricsById", data);
+            // console.log({ data });
             // alert(data.data.mus[0].text)
             return data.data.mus[0].text;
         })
@@ -37,19 +35,15 @@ const getLyricsById = async (id) => {
 
 const getLyricsByTrecho = async (trecho) => {
 
-
-
     const queryByTrecho = `https://api.vagalume.com.br/search.excerpt?q=${trecho}&extra=relmus&apikey=${key}`
-
-
 
     return await axios
         .get(
             queryByTrecho
         )
         .then(data => {
-            console.log("getLyricsByTrecho", data);
-            console.log(data.data.response.docs[0].id)
+            // console.log("getLyricsByTrecho", data);
+            // console.log(data.data.response.docs[0].id)
             return getLyricsById(data.data.response.docs[0].id)
 
         })
@@ -65,11 +59,10 @@ class Lyrics extends Component {
         letra: ''
     }
 
-    handleOnSubmit = event => {
-        event.preventDefault()
-        const trecho = event.target.idSearch.value
-        // Não permitir ir na API para buscar uma palavra tão simples
-        this.setState({ letra: 'Procurando...'})
+    handleOnChange = event => {
+        const trecho = event.target.value
+        this.setState({ letra: 'Procurando...' })
+         // Não permitir ir na API para buscar uma palavra tão simples
         if (trecho.length <= 3) {
             return
         }
@@ -80,14 +73,12 @@ class Lyrics extends Component {
 
     showLetra = async () => {
         const letra = await getLyricsByTrecho(this.state.trecho)
-        await console.log('letra buscada', letra)
         this.setState({ letra });
     };
 
     render() {
 
         const { classes } = this.props;
-
 
         return (
             <Fragment>
@@ -110,21 +101,20 @@ class Lyrics extends Component {
                     <br /><br />
                 </div>
                 <div className="container">
-                    <Form layout="vertical" onSubmit={this.handleOnSubmit}>
+                    <Form layout="vertical">
 
                         <FormItem>
-                            <Search id="idSearch"
+                            <Search 
                                 placeholder="digite um trecho da música..."
+                                onChange={this.handleOnChange}
                             />
 
-                          { this.state.trecho && <TextArea autosize value={this.state.letra} /> }
+                            {this.state.trecho && <TextArea autosize value={this.state.letra} />}
                         </FormItem>
 
                     </Form>
                 </div>
-                {/* {getLyricsByTrecho()} */}
-
-
+               
                 <Fade duration={3000}>
                     <Footer />
                 </Fade>
